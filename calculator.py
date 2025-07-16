590a5bd571fe202b07eae3ed44c7e4b64a64c28a
Sasha-Tameka <sashh92@hotmail.com>
2025-07-16 15:43:18 -0400

    added number validation

--- calculator.py
+++ calculator.py
@@ -1,16 +1,19 @@
 #user input (first number, second number, operators)
+try:
+    first_num = float(input("Enter your first number: "))
+    second_num = float(input("Enter your second number: "))
+    operator = input("Enter operator (+, -, *, /): ")
 
-first_num = float(input("Enter your fist number: "))
-second_num = float(input("Enter your second number: "))
-operator = input("Enter operator: ")
+    if operator == "+":
+        print(first_num + second_num)
+    elif operator == "-":
+        print(first_num - second_num)
+    elif operator == "*":
+        print(first_num * second_num)
+    elif operator == "/":
+        print(first_num / second_num)
+    else:
+        print("Error, Invalid operator")
 
-if operator == "+":
-    print(first_num + second_num)
-elif operator == "-":
-    print(first_num - second_num)
-elif operator == "*":
-    print(first_num * second_num)
-elif operator == "/":
-    print(first_num / second_num)
-else:
-    print("Error")
+except ValueError:
+    print("Please enter valid number")
