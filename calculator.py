97fbbb56c30ad1a680c4e510ffe75e7e9f02aa1c
Sasha-Tameka <sashh92@hotmail.com>
2025-07-16 15:15:10 -0400

    #user input (first number, second number, operators)

--- calculator.py
+++ calculator.py
@@ -1,1 +1,5 @@
-print("Hello World")
+#user input (first number, second number, operators)
+
+first_num = input("Enter your fist number: ")
+second_num = input("Enter your second number: ")
+operator = input("Enter your operator: ")
