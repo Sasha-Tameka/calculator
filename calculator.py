fa2f70df6d083b4cde0d05419e082aba4b0a3f16
Sasha-Tameka <sashh92@hotmail.com>
2025-07-16 15:27:47 -0400

    added conditions

--- calculator.py
+++ calculator.py
@@ -3,3 +3,14 @@
 first_num = float(input("Enter your fist number: "))
 second_num = float(input("Enter your second number: "))
 operator = input("Enter operator: ")
+
+if operator == "+":
+    print(first_num + second_num)
+elif operator == "-":
+    print(first_num - second_num)
+elif operator == "*":
+    print(first_num * second_num)
+elif operator == "/":
+    print(first_num / second_num)
+else:
+    print("Error")
