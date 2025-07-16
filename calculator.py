2d4cfeedbfdf69afb42c9b716f6e252251aa204b
Sasha-Tameka <sashh92@hotmail.com>
2025-07-16 16:07:11 -0400

    added while loop and f format

--- calculator.py
+++ calculator.py
@@ -1,19 +1,22 @@
 #user input (first number, second number, operators)
-try:
-    first_num = float(input("Enter your first number: "))
-    second_num = float(input("Enter your second number: "))
-    operator = input("Enter operator (+, -, *, /): ")
 
-    if operator == "+":
-        print(first_num + second_num)
-    elif operator == "-":
-        print(first_num - second_num)
-    elif operator == "*":
-        print(first_num * second_num)
-    elif operator == "/":
-        print(first_num / second_num)
-    else:
-        print("Error, Invalid operator")
 
-except ValueError:
-    print("Please enter valid number")
+while True:
+    try:
+        first_num = float(input("Enter your first number: "))
+        second_num = float(input("Enter your second number: "))
+        operator = input("Enter operator (+, -, *, /): ")
+
+        if operator == "+":
+            print(f"Answer: {first_num + second_num}")
+        elif operator == "-":
+            print(f"Answer:  {first_num - second_num}")
+        elif operator == "*":
+            print(f"Answer: {first_num * second_num}")
+        elif operator == "/":
+            print(f"Answer: {first_num / second_num}")
+        else:
+            print("Error, Invalid operator")
+
+    except ValueError:
+        print("Please enter valid number")
